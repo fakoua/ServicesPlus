54b9d2b58d7893f0ec21290be252dcb4e6545677
Sameh Fakoua <sameh.osd@novopath.com>
2019-07-30 19:41:23 +0300

    Code Cleanup

---
+++ ServicesPlus/My Project/AssemblyInfo.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Resources
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Runtime.CompilerServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Services+")> 
+<Assembly: AssemblyDescription("Advanced Service Manager")> 
+<Assembly: AssemblyCompany("Services+")> 
+<Assembly: AssemblyProduct("Services+")> 
+<Assembly: AssemblyCopyright("Copyright © Sameh Fakoua 2014")> 
+<Assembly: AssemblyTrademark("Services+")> 
+
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("89964456-d065-4495-a95e-f0017498f0da")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.8")>
+<Assembly: AssemblyFileVersion("1.0.0.8")>
+
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
